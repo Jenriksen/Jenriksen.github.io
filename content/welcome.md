f276643d7455fcf495dc45f78ca06c7d04ef25af
Jesper Henriksen <jesperhen89@gmail.com>
2024-03-23 05:17:23 +0100

    setup wip

---
+++ content/welcome.md
@@ -0,0 +1,9 @@
+Title: Welcome All
+Slug: welcome-all
+Date: 2024-03-22 05:39
+Category: Python
+Tags: pelican, publishing
+Author: Josef Jezek
+Summary: Short version for index and feeds
+
+This is the content of my blog post.
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+AUTHOR = 'Jesper Henriksen'
+SITENAME = "Jenriksen's personal ramblings"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'EN'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = 'themes/bootstrap2'
+OUTPUT_PATH = 'output'
+PATH = 'content'
+
+# Custom Home page
+DIRECT_TEMPLATES = (('index', 'blog', 'tags', 'categories', 'archives'))
+PAGINATED_DIRECT_TEMPLATES = (('blog',))
+TEMPLATE_PAGES = {'home.html': 'index.html',}
